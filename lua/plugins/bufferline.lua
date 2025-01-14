2d589251b566d377ffcfaa3e93409fbe7664b411
bothadam <adam@linebooker-tech.com>
2025-01-14 16:11:00 +0200

    make tabs more readable and take away close buttons

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      max_name_length = 100,           -- Set the maximum number of characters for a buffer name
+      max_width = 100,                 -- Set the maximum width of a tab
+      show_buffer_close_icons = false, -- Remove close icons to save space
+      separator_style = "thin",        -- Use a thin separator for a cleaner look
+    },
+  },
+}
