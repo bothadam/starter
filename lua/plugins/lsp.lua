3cd26702a827bf66dc453f169a8dd2588fa2faf1
bothadam <adambotha007@gmail.com>
2025-04-23 12:13:53 +0200

    setup lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,5 +26,26 @@
 --     },
 --   },
 -- }
---
-return {}
+
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = { "omnisharp" },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        omnisharp = {
+          -- optional: fix path if you're using global install or custom mason install
+          -- cmd = { "/path/to/omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+          enable_roslyn_analyzers = true,
+          organize_imports_on_format = true,
+          enable_import_completion = true,
+        },
+      },
+    },
+  },
+}
